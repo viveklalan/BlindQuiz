5ad65325d6e9f5743c3e5c28bc7c89965d9a629a
Saurabh CG <saurabh@cloudganga.com>
2017-01-23 14:58:27 +0530

    Initial Commit

---
+++ gkBlind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gkBlind
+//
+//  Created by CG-3 on 23/01/17.
+//  Copyright © 2017 CG-3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
